6f71e132017d8b105da30338e87192ea3b36356b
Habeebuddin Huzaifa <71178360+habeebuddinhuzaifa@users.noreply.github.com>
2020-09-12 18:16:36 +0530

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,49 @@
+<section class="page-header page-header-xs">
+
+				<div class="container">
+
+					<a href="http://milliservices.com/"><img src="http://milliservices.com/images/logo.png" class="img-responsive center-block" style="width:200px; height:auto; align:center;" alt="Milli Services"></a>
+
+					
+
+
+
+					<!-- breadcrumbs -->
+
+					<ol class="breadcrumb">
+
+						<!--<li><a href="index.php">Home</a></li>--!>
+
+						<!--<li><a href="about.php">About</a></li>
+
+						<li><a href="#" class=" dropdown-toggle" data-toggle="dropdown">Participants</a>
+
+							<ul class="dropdown-menu">
+
+							  <li><a href="gulbarga.php">Gulbarga</a></li>
+
+							  <li></li>
+
+							  <li><a href="mbnr.php">Mahabubnagar</a></li>
+
+							</ul>
+
+						</li>
+
+						<li><a href="campaign.php">Campaign</a></li>
+
+						<li><a href="trainer.php">Trainers</a></li>
+
+						<li><a href="gallery.php">Gallery</a></li>-->
+
+					</ol>
+
+					<!-- /breadcrumbs -->
+
+
+
+
+
+				</div>
+
+			</section>
